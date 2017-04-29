9d089bfe714591dcb987fb58fc1b0b342d1c37a0
Judicaël Grasset <judicael@posteo.de>
2017-04-29 21:04:14 +0200

    Minor enhancements in code source style

--- ot.pl
+++ ot.pl
@@ -13,6 +13,7 @@ use File::Find;
 use File::HomeDir;
 use Encode qw(decode);
 use Getopt::Long qw(:config no_auto_abbrev);
+use English qw(-no_match_vars);
 
 sub generate_db {
     my @directories_to_search = (File::HomeDir->my_home);
@@ -25,15 +26,15 @@ sub generate_db {
     sub wanted_closure {
         my @found = ();
 
-        my $finder = sub { push @found, decode('UTF-8', $File::Find::name)};
-        my $results = sub {@found};
+        my $finder = sub { push @found, decode('UTF-8', $File::Find::name) };
+        my $results = sub { @found };
 
         return ($finder, $results);
     }
 
     my ($wanted, $list_of_files) = wanted_closure();
 
-    find({preprocess => \&preprocess, wanted => $wanted }, @directories_to_search);
+    find({ preprocess => \&preprocess, wanted => $wanted }, @directories_to_search);
     return $list_of_files->();
 }
 
@@ -54,13 +55,13 @@ sub get_list_of_match {
     my @results;
 
     #Push regex into the arrays
-    foreach my $word ( @args ) {
+    foreach my $word (@args) {
         push @matches_anywhere,qr/$word/i;
         push @matches_in_filename, qr/$word[^\/]*$/i;
     }
 
     foreach my $file (@list_of_files) {
-        if ( all {$file =~ $_ } @matches_anywhere) {
+        if ( all { $file =~ $_  } @matches_anywhere) {
             if ( any { $file =~ $_ } @matches_in_filename ) {
                 push @results,$file
             }
@@ -71,38 +72,40 @@ sub get_list_of_match {
 
 sub usage {
     print "usage: ot [--generate] [--db file]\n";
+    return;
 }
 
 sub ot {
-    my $file_db = "";
+    my $file_db = q{};
     my $generate = 0;
-    GetOptions( "db=s" => \$file_db, "generate" => \$generate );
+    GetOptions( 'db=s' => \$file_db, 'generate' => \$generate );
 
-    if ($generate == 1 ) {
-        if ( $file_db eq "" ) {
+    if ( $generate == 1 ) {
+        if ( not $file_db ) {
             print "Output file is missing.\n";
             print "You must specify where to generate it.\n";
             exit 1;
         } else {
             print "want a refresh\n";
             my @list_of_files = generate_db();
-            open ( my $output_file, '>', $file_db ) or die "Cannot open $file_db $!\n";
+            open ( my $output_file, '>', $file_db ) or die "Cannot open $file_db $OS_ERROR\n";
             foreach my $l (@list_of_files) {
                 print $output_file "$l\n";
             }
-            close($output_file);
+            close($output_file) or die "Cannot close $file_db $OS_ERROR\n";
         }
     } else {
-        if ( $file_db eq "" ) {
+        if ( not $file_db ) {
             usage();
             exit 1;
         } else {
-            open ( my $input_file, '<', $file_db ) or die "Cannot open $file_db $!\n";
+            open ( my $input_file, '<', $file_db ) or die "Cannot open $file_db $OS_ERROR\n";
             chomp( my @list_of_files = <$input_file> );
-            close($input_file);
+            close($input_file) or die "Cannot close $file_db $OS_ERROR\n";
             gtk(@list_of_files);
         }
     }
+    return;
 }
 
 sub gtk{
@@ -114,18 +117,19 @@ sub gtk{
     my $window = Gtk3::Window->new ('toplevel');
     $window->signal_connect (delete_event => sub { Gtk3->main_quit });
 
-    my $box = Gtk3::Box->new("vertical", 2);
+    my $box = Gtk3::Box->new('vertical', 2);
     my $search_bar = Gtk3::SearchBar->new();
     my $search_entry = Gtk3::SearchEntry->new();
     my $scrolledwindow = Gtk3::ScrolledWindow->new();
     my $listbox = Gtk3::ListBox->new();
 
-    foreach(my $i = 0; $i < $max_matches; $i++){
-        push @labels, Gtk3::Label->new("");
+    foreach(my $i = 0; $i < $max_matches; $i++) {
+        push @labels, Gtk3::Label->new('');
         $listbox->insert($labels[$i], -1);
     };
 
-    $listbox->set_filter_func(sub {
+    $listbox->set_filter_func(
+        sub {
             my $row = $_[0];
             return ($num_matches != 0 && $row->get_index() < $num_matches);
         }
@@ -138,7 +142,8 @@ sub gtk{
     $search_bar->add($search_entry);
     $search_bar->connect_entry($search_entry);
     $search_bar->set_search_mode(TRUE);
-    $search_entry->signal_connect("search-changed" => sub {
+    $search_entry->signal_connect('search-changed' =>
+        sub {
             my $string_of_args = $search_entry->get_text();
             my @args = split(' ', $string_of_args);
             print "[@args]\n";
@@ -153,7 +158,7 @@ sub gtk{
         }
     );
 
-    $listbox->signal_connect("row-activated" => sub {
+    $listbox->signal_connect('row-activated' => sub {
             my $row = $_[1];
             my $label = $row->get_child();
             my $selected_file = $label->get_text();
